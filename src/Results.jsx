cce299459d76c98ab71989dfcca55ee16411427e
MaPaulaMV <mapaula.monteros@gmail.com>
2024-12-26 01:48:35 -0500

    fix results component

--- src/Results.jsx
+++ src/Results.jsx
@@ -7,14 +7,16 @@ const Results = ({ pets }) => {
         <h1>No Pets Found</h1>
       ) : (
         pets.map((pet) => {
-          <Pet
-            animal={pet.animal}
-            name={pet.name}
-            breed={pet.breed}
-            images={pet.images}
-            location={`${pet.city}, ${pet.state}`}
-            key={pet.id}
-          />;
+          return (
+            <Pet
+              animal={pet.animal}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              key={pet.id}
+            />
+          );
         })
       )}
     </div>

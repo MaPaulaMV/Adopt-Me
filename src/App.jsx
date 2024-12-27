7d9d838dca642201bec46a897a15b85ead12fd41
MaPaulaMV <mapaula.monteros@gmail.com>
2024-12-26 20:39:28 -0500

    react router and query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   // return React.createElement(
@@ -26,10 +38,17 @@ const App = () => {
   // );
 
   return (
-    <div>
-      <h1>Adop Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
